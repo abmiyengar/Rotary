a5362ab1b3d31c5c201281334cd9ee7eaec38a82
Abhishek M Iyengar <abhi.ingr@gmail.com>
2016-07-08 17:01:20 -0400

    Rotary Project
    Larval Setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Gallery</div>
+
+                <div class="panel-body">
+  
+
+  <div class="row">
+    <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/7b126d33bd57dba9942b42577af1e131.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/492cf958c15e2f75890a40c723b20ca5.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/2012.11-Banner-Amarillo-Texas-Rotary-sm.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/7b126d33bd57dba9942b42577af1e131.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/492cf958c15e2f75890a40c723b20ca5.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/2012.11-Banner-Amarillo-Texas-Rotary-sm.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/492cf958c15e2f75890a40c723b20ca5.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/2012.11-Banner-Amarillo-Texas-Rotary-sm.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+     <div class="col-lg-4 col-sm-6 col-xs-12">
+        <a href="#">
+             <img src="banners/7b126d33bd57dba9942b42577af1e131.jpg" class="thumbnail img-responsive">
+        </a>
+    </div>
+  </div>
+</div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
